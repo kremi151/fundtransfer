71631a189d5d4af75f5cb8e58993eced31b4d301
Michel Kremer <kremi151dev@gmail.com>
2024-03-06 16:49:17 +0100

    Bootstrap project

---
+++ src/main/kotlin/lu/mkremer/fundstransfer/FundstransferApplication.kt
@@ -0,0 +1,11 @@
+package lu.mkremer.fundstransfer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FundstransferApplication
+
+fun main(args: Array<String>) {
+	runApplication<FundstransferApplication>(*args)
+}
