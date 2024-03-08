c24ff805b7ff74b9cfad7d3c186dcd280121d628
Michel Kremer <kremi151dev@gmail.com>
2024-03-08 23:36:32 +0100

    Improve validation and documentation in AccountController

--- src/main/kotlin/lu/mkremer/fundstransfer/controller/AccountController.kt
+++ src/main/kotlin/lu/mkremer/fundstransfer/controller/AccountController.kt
@@ -2,18 +2,20 @@ package lu.mkremer.fundstransfer.controller
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import lu.mkremer.fundstransfer.datamodel.dto.AccountDTO
+import lu.mkremer.fundstransfer.datamodel.dto.ValidationErrorDTO
 import lu.mkremer.fundstransfer.datamodel.request.CreateAccountRequest
 import lu.mkremer.fundstransfer.service.AccountService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @Tag(
@@ -28,12 +30,54 @@ class AccountController {
 
     @PostMapping
     @Operation(summary = "Creates a new account")
-    fun createAccount(@RequestBody request: CreateAccountRequest): AccountDTO {
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "201",
+                description = "The account was created",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        schema = Schema(implementation = AccountDTO::class),
+                    ),
+                ],
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Invalid input supplied",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        schema = Schema(implementation = ValidationErrorDTO::class),
+                    ),
+                ],
+            ),
+        ])
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createAccount(@Valid @RequestBody request: CreateAccountRequest): AccountDTO {
         return accountService.createAccount(request)
     }
 
     @GetMapping("/{id}")
     @Operation(summary = "Retrieves information about an account")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "The account was found",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        schema = Schema(implementation = AccountDTO::class),
+                    ),
+                ],
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "The account was not found",
+                content = [Content()],
+            ),
+        ])
     fun getAccount(
         @Parameter(description = "The account ID") @PathVariable("id") id: Int,
     ): ResponseEntity<AccountDTO> {
--- src/main/kotlin/lu/mkremer/fundstransfer/datamodel/request/CreateAccountRequest.kt
+++ src/main/kotlin/lu/mkremer/fundstransfer/datamodel/request/CreateAccountRequest.kt
@@ -1,9 +1,11 @@
 package lu.mkremer.fundstransfer.datamodel.request
 
 import io.swagger.v3.oas.annotations.media.Schema
+import lu.mkremer.fundstransfer.validation.Currency
 
 @Schema(description = "The request for creating a new account")
 class CreateAccountRequest(
     @Schema(description = "The currency of the money that this account will hold", required = true)
+    @Currency // TODO: Test validation
     val currency: String,
 )
